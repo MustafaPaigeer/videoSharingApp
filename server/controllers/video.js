b6ab6e2c5fc3e6ed31fe99aecf3b3e5955ef605d
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-02 13:25:45 -0700

    Add get video function to video contoller

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -49,6 +49,15 @@ export const deleteVideo = async (req, res, next) => {
 
 export const getVideo = async (req, res, next) => {
   try {
+    const video = await Video.findById(req.params.id)
+    res.status(200).json(video)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const addView = async (req, res, next) => {
+  try {
 
   } catch (error) {
     next(error)
