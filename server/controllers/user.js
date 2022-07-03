57fe8918abeb67c9ebaf619bea7c07d7d8e76168
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-03 14:10:13 -0700

    add subscribe and unsubscribe functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -44,14 +44,30 @@ export const getUser = async (req, res, next) => {
 
 export const subscribe = async (req, res, next) => {
   try {
-     
+     await User.findById(req.user.id, {
+      $push:{subscribedUsers: req.params.id}
+     })
+     await User.findByIdAndUpdate(req.params.id, {
+      $inc: {subscribers: 1}
+     });
+     res.status(200).json({message: "subscription successful."})
   } catch (error) {
     next(error)
   }
 }
 export const unsubscribe = async (req, res, next) => {
   try {
-
+    try {
+      await User.findById(req.user.id, {
+       $pull:{subscribedUsers: req.params.id}
+      })
+      await User.findByIdAndUpdate(req.params.id, {
+       $inc: {subscribers: -1}
+      });
+      res.status(200).json({message: "Unsubscription successful."})
+   } catch (error) {
+     next(error)
+   }
   } catch (error) {
     next(error)
   }
