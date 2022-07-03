e69d4073cdc06a335a840163a472770f7556425b
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-03 14:18:13 -0700

    add create video function to video controller

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,16 +1,34 @@
 
 export const addVideo = async (req, res, next) => {
-
+  const newVideo = new Video({userId: req.user.id, ... req.body});
+  try {
+    const savedVideo = await newVideo.save()
+    res.status(200).json({video: savedVideo})
+  }catch (error){
+    next(error)
+  }
 }
 
 export const updateVideo = async (req, res, next) => {
-  
+  try {
+
+  }catch (error){
+    next(error)
+  }
 }
 
 export const deleteVideo = async (req, res, next) => {
-  
+  try {
+
+  }catch (error){
+    next(error)
+  }
 }
 
 export const getVideo = async (req, res, next) => {
-  
+  try {
+
+  }catch (error){
+    next(error)
+  }
 }
