ddb231ef1cbfa1a147fdc6c083cde4e792d81035
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-06 14:05:33 -0700

    add delete user functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -19,21 +19,35 @@ export const update = async (req, res, next) => {
     return next(createError(403, "You can update only your account!"))
   }
 }
-export const deleteUser = (req, res, next) => {
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: 'User has been deleted' })
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(createError(403, "You can update only your account!"))
+  }
+}
+
+export const getUser = async (req, res, next) => {
 
 }
-export const getUser = (req, res, next) => {
+
+export const subscribe = async (req, res, next) => {
 
 }
-export const subscribe = (req, res, next) => {
+export const unsubscribe = async (req, res, next) => {
 
 }
-export const unsubscribe = (req, res, next) => {
+
+export const like = async (req, res, next) => {
 
 }
-export const like = (req, res, next) => {
 
-}
-export const dislike = (req, res, next) => {
+export const dislike = async (req, res, next) => {
 
 }
