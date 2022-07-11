3ed87ddea6940542868627026bf04663bd324706
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-10 17:05:33 -0700

    add update user functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,20 @@
-import { createError } from "../error"
+import { createError } from "../error.js";
+import User from "../models/User.js";
 
-export const update = (req, res, next) => {
-  if(req.params.id === req.user.id) {
-    // update
+export const update = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id,
+        {
+          $set: req.body
+        },
+        {
+          new: true
+        })
+      res.status(200).json(updatedUser)
+    } catch (error) {
+      next(error)
+    }
   } else {
     return next(createError(403, "You can update only your account!"))
   }
@@ -16,7 +28,7 @@ export const getUser = (req, res, next) => {
 export const subscribe = (req, res, next) => {
 
 }
-export const unsubscribe= (req, res, next) => {
+export const unsubscribe = (req, res, next) => {
 
 }
 export const like = (req, res, next) => {
