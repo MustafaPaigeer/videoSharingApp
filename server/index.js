082578b9a0aad7e6a8b9cb9cceac1339a1ac112f
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-13 14:12:30 -0700

    create express and connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
+dotenv.config()
+
+const connect = () => {
+  mongoose.connect(process.env.MONGO_URI)
+  .then(()=> {
+    console.log("connected to DB")
+  })
+  .catch ((error) => {
+    throw error;
+  })
+}
 
 app.listen(3001, () => {
+  connect()
   console.log("Connected..")
 })
