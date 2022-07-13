fa0259992de45e67c55c6aa5b7f5004de117103f
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-13 14:27:17 -0700

    Create user, video and comment models

---
+++ server/models/Video.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required:true,
+  },
+  title: {
+    type: String,
+    required:true,
+  },
+  description: {
+    type:String,
+    required:true,
+  },
+  imgUrl: {
+    type:String,
+    required:true,
+  },
+  videoUrl: {
+    type:String,
+    required:true,
+  },
+  views: {
+    type:Number,
+    default:0,
+  },
+  tags: {
+    type: [String],
+    default:[]
+  },
+},
+  {timestamps: true}
+);
+
+export default mongoose.model("Video", VideoSchema);
