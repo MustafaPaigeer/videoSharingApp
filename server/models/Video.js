a4cca55bfce2f2ae801a698ac8554a0cdf506d9c
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-13 23:55:57 -0700

    setup user route tests

--- server/models/Video.js
+++ server/models/Video.js
@@ -3,34 +3,34 @@ import mongoose from "mongoose";
 const VideoSchema = new mongoose.Schema({
   userId: {
     type: String,
-    required:true,
+    required: true,
   },
   title: {
     type: String,
-    required:true,
+    required: true,
   },
   description: {
-    type:String,
-    required:true,
+    type: String,
+    required: true,
   },
   imgUrl: {
-    type:String,
-    required:true,
+    type: String,
+    required: true,
   },
   videoUrl: {
-    type:String,
-    required:true,
+    type: String,
+    required: true,
   },
   views: {
-    type:Number,
-    default:0,
+    type: Number,
+    default: 0,
   },
   tags: {
     type: [String],
-    default:[]
+    default: []
   },
 },
-  {timestamps: true}
+  { timestamps: true }
 );
 
 export default mongoose.model("Video", VideoSchema);
