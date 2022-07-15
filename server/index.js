4bcb3017c513044055414fbc254316ec2940714d
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-14 20:18:47 -0700

    basic route test and setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import userRoutes from "./routes/users.js"
 
 const app = express()
 dotenv.config()
@@ -15,7 +16,9 @@ const connect = () => {
   })
 }
 
-app.listen(3001, () => {
+app.use("/api/users", userRoutes)
+
+app.listen(3000, () => {
   connect()
-  console.log("Connected..")
+  console.log("Connected to server")
 })
---
+++ server/routes/auth.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {  } from "../controllers/user.js";
+
+const router = express.Router();
+
+// Create a user
+router.post("/signup", )
+router.post("/signup", )
+
+// Sign In
+
+
+// Google Authentication
+
+
+
+export default router;
