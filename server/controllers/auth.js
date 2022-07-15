b2df6bb37b5164dc4e9a6f9f802fc2b2426b8fe7
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-14 23:33:24 -0700

    create error handler fucntion

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import { createError } from "../error.js";
 
 export const signup = async (req,res, next)=>{
   try {
@@ -11,6 +12,6 @@ export const signup = async (req,res, next)=>{
     await newUser.save()
     res.status(200).send("User has been created")
   }catch (err) {
-    next(err)
+    next(createError(404, "not found sorry!"))
   }
-}
+};
