0401e9d5a65172133bf789390cd2afd1f0c72b78
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-15 23:24:55 -0700

    debuggin express

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,16 +22,17 @@ export const signin = async (req, res, next) => {
     const user = await User.findOne({ name: req.body.name })
     if (!user) return next(createError(404, "User not found!"))
     const isCorrect = await bcrypt.compare(req.body.password, user.password)
-    if(!isCorrect) return next(createError(400, "Worng credentials!"))
+    if (!isCorrect) return next(createError(400, "Worng credentials!"));
 
-    const token = jwt.sign({id: user._id}, process.env.JWT)
-    const {password, ...other} = user._doc
-    res.cookie("access_token", token, {
-      httpOnly:true
-    }).status(200).json(others)
-
+    // const token = jwt.sign({ id: user._id }, process.env.JWT);
+    // const { password, ...others } = user._doc
+    // res
+    //   .cookie("access_token", token, {
+    //     httpOnly: true
+    //   }).status(200)
+    //   .json(others);
 
   } catch (err) {
-    next(err)
+    next(err);
   }
 }; 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import userRoutes from "./routes/users.js"
-import authRoute from "./routes/auth.js"
-import videoRoute from "./routes/videos.js"
-import commentRoute from "./routes/comments.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import cookieParser from "cookie-parser";
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
 const connect = () => {
   mongoose.connect(process.env.MONGO_URI)
@@ -20,24 +20,37 @@ const connect = () => {
   });
 };
 
-app.use(cookieParser)
-app.use(express.json())
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoutes)
-app.use("/api/vidoes", videoRoute)
-app.use("/api/comments", commentRoute)
+//middlewares
+app.use(cookieParser);
+app.use(express.json());
 
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
-  return res.status(status).json({
-    success: false,
-    status,
-    message
-  })
+//routes
+
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
 
-app.listen(8800, () => {
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/vidoes", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+
+//error handler
+// app.use((err, req, res, next) => {
+//   const status = err.status || 500;
+//   const message = err.message || "Something went wrong!";
+//   return res.status(status).json({
+//     success: false,
+//     status,
+//     message,
+//   });
+// });
+
+const port = process.env.PORT || 8800;
+
+app.listen(port, () => {
   connect()
-  console.log(`Server is listening on port 3001`)
+  console.log(`Server is listening on port ${port}`)
 })
