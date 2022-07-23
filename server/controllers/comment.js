888f1f457d6a76e2504ef865a435bea64085e4b1
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-23 14:10:10 -0700

    add delete comment functionality

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,26 +1,37 @@
+import { createError } from "../error.js";
+import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
 
-export const addComment = async(req,res,next) => {
-  const newComment = new Comment({ ...req.body, userId: req.user.id})
+
+export const addComment = async (req, res, next) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id })
   try {
     const savedComment = await Comment.save()
-    res.status(200).json({comment: comment})
-  }catch (error) {
+    res.status(200).json({ comment: savedComment })
+  } catch (error) {
     next(error)
   }
 }
 
-export const deleteComment = async(req,res,next) => {
+export const deleteComment = async (req, res, next) => {
   try {
-
-  }catch (error) {
-
+    const comment = await Comment.findById(res.params.id);
+    const video = await Video.findById(res.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment has been deleted.");
+    } else {
+      return next(createError(403, "You can delete ony your comment!"));
+    }
+  } catch (error) {
+    next(error);
   }
 }
 
-export const getComment = async(req,res,next) => {
+export const getComment = async (req, res, next) => {
   try {
 
-  }catch (error) {
+  } catch (error) {
 
   }
 }
