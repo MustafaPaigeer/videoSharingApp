3974dbccd991b631d45bfb984bbba641fabce398
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-08 14:22:33 -0700

    Add comments component

--- clientUI/src/Components/Comments.jsx
+++ clientUI/src/Components/Comments.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Comment from "./Comment";
 
-const Container = styled.div`
+const Container = styled.div``;
+
+const NewComment = styled.div`
   display: flex;
+  align-items: center;
   gap: 10px;
-  margin: 30px 0px;
 `;
 
 const Avatar = styled.img`
@@ -13,45 +16,32 @@ const Avatar = styled.img`
   border-radius: 50%;
 `;
 
-const Details = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  color: ${({ theme }) => theme.text}
-`;
-const Name = styled.span`
-  font-size: 13px;
-  font-weight: 500;
-`;
-
-const Date = styled.span`
-  font-size: 12px;
-  font-weight: 400;
-  color: ${({ theme }) => theme.textSoft};
-  margin-left: 5px;
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
 `;
 
-const Text = styled.span`
-  font-size: 14px;
-`;
-
-const Comment = () => {
+const Comments = () => {
   return (
     <Container>
-      <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
-      <Details>
-        <Name>
-          John Doe <Date>1 day ago</Date>
-        </Name>
-        <Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
-          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
-          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
-          mollitia totam reiciendis?
-        </Text>
-      </Details>
+      <NewComment>
+        <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+        <Input placeholder="Add a comment..." />
+      </NewComment>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
     </Container>
   );
 };
 
-export default Comment;
+export default Comments;
