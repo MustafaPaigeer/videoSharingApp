aabc5bb05de5f5e9f738f116c2d9ef1c794689ea
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-08 23:53:18 -0700

    Adding style to left side menu

--- clientUI/src/Components/Menu.jsx
+++ clientUI/src/Components/Menu.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
 flex: 1;
-background-color: #202020;
+background-color: ${({ theme }) => theme.bgLighter};
+height: 100vh;
+color: ${({ theme }) => theme.text};
+font-size: 14px;
+position: sticky;
+top: 0;
 `;
-const Main = styled.div`
-  flex: 7;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
 `;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
 const Menu = () => {
   return (
     <div>Menu</div>
